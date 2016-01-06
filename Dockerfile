eb517072e30e5ab15f5ad1179da56e7e944b05d7
Graham McGregor <dev@grahammcgregor.ca>
2016-01-05 21:43:09 -0500

    Update init script to use psql
    The upstream postgres docker image now starts the database before
    sourcing scripts. So don't need to use single user mode, can use regular
    psql command.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM postgres:9.4
-RUN mkdir /scripts
+RUN mkdir -p /scripts
+RUN mkdir -p /docker-entrypoint-initdb.d
 ADD create_db.sql /scripts/
 ADD create_schema.sql /scripts/
 ADD create_views.sql /scripts/
--- init.sh
+++ init.sh
@@ -3,10 +3,10 @@
 sed -ri "s/host all all 0.0.0.0\/0 trust/host all all 0.0.0.0\/0 md5/" "$PGDATA"/pg_hba.conf
 
 echo Creating db...
-gosu postgres postgres --single < /scripts/create_db.sql
+psql --username "$POSTGRES_USER" < /scripts/create_db.sql
 echo Creating schema...
-gosu postgres postgres --single qcumberdb -j < /scripts/create_schema.sql
-gosu postgres postgres --single qcumberdb -j < /scripts/create_views.sql
+psql --username "$POSTGRES_USER" qcumberdb < /scripts/create_schema.sql
+psql --username "$POSTGRES_USER" qcumberdb < /scripts/create_views.sql
 if [ "$?" == 0 ]; then
     echo Sucess!
 else
